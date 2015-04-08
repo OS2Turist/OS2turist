93ac57bdf9f37e856f8525b30656f5c7f23028a9
Claus Stilborg <cb@nebulators.com>
2015-04-08 12:30:10 +0200

    polished the slider functionality and included the window titel in showing the current range

--- app/controllers/details.js
+++ app/controllers/details.js
@@ -1,1 +1,7 @@
 var args = arguments[0] || {};
+
+function doClose(e){
+	args = null;
+	$.off();
+	$.destroy();
+}
--- app/controllers/home.js
+++ app/controllers/home.js
@@ -1,11 +1,10 @@
 var args = arguments[0] || {};
 
 function doChangeKm(e){
-	$.lblKmSetting.text = String.format("%d", e.value) + " km";
+	$.lblKmSetting.text = String.format("%d", e.value) + " KM";
+	$.win.title = L('hometitle') + " " + String.format("%d", e.value) + " KM";
 }
 
 (function(){
-	
-	 // %d
-	$.lblKmSetting.text = String.format("%d", $.sldKmSetting.value) + " km";	
+	doChangeKm($.sldKmSetting);	
 })();
